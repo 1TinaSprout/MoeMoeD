6f4b4aaa98a98f2b00a115f2cd7977d0f519b622
匀强的磁场 <297498075@users.noreply.github.com>
2017-11-01 11:21:30 +0800

    update 1.2

--- MoeMoeD/Scripts/Home.js
+++ MoeMoeD/Scripts/Home.js
@@ -1,9 +1,31 @@
 ﻿var app = new Vue({
     el : "#app",
-    data : {
+    data: {
+        create_folderName: "",
+        search_content: "",
+        create_folder: false,
+        count:0,
+        content_columns: [{
+            title: "文件名",
+            key:"Name"
+        }, {
+            title: "大小",
+            key:"Size"
+        }, {
+            title: "修改日期",
+            key:"UpdateTime"
+        }],
+        content_data: [{
+            Name: "我的文件",
+            Size: "100K",
+            UpdateTime: "2017-01-01"
+        }]
     },
     methods : {
         select: function (e) {
+
+        },
+        createfolder: function (e) {
 
         }
     }
