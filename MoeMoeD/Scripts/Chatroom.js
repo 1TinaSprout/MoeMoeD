ecdd38ee0468fd3c1ad436993ecdcaafb6507f70
匀强的磁场 <297498075@users.noreply.github.com>
2017-11-01 16:38:09 +0800

    update 1.3

---
+++ MoeMoeD/Scripts/Chatroom.js
@@ -0,0 +1,17 @@
+﻿var app = new Vue({
+    el: "#app",
+    data: {
+        userName: "用户名",
+        userSign: "个性化签名",
+        roomName: "会话窗口名",
+        work_list: [{
+            name: "111",
+            nickName: "222"
+        }, {
+            name: "333",
+            nickName: "444"
+        }]
+    },
+    methods: {
+    }
+})
