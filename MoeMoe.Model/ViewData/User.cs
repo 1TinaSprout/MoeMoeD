109d246c72fd5a4af1ff517320b75911a129ed5a
匀强的磁场 <297498075@users.noreply.github.com>
2017-11-02 21:30:03 +0800

    update1.6

--- MoeMoe.Model/ViewData/User.cs
+++ MoeMoe.Model/ViewData/User.cs
@@ -6,8 +6,9 @@ using System.Threading.Tasks;
 
 namespace MoeMoeD.Model.ViewData
 {
-    public class User : BaseData
+    public class User
     {
+        public int Id { get; set; }
         public String Name { get; set; }
         public String Email { get; set; }
         public String Password { get; set; }
